37910a13ac7141480e57eb7fa421b6b83b552045
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-24 17:25:58 +0200

    Created Home Component and CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import Home from "./Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Home />
+
+      {/*Home Component:*/}
+      {/*Header*/}
+
+      {/*Banner (with Search)*/}
+
+      {/*Cards*/}
+
+      {/*Footer*/}
+
+      {/*SearchPage Component*/}
+    </div>
+  );
 }
 
 export default App;
