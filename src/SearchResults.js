d9ce140d1f15fe40ce6cd99e17bb83d47efdeef3
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-25 15:30:40 +0200

    Added SearchResults Content and Styling

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,8 +1,43 @@
 import React from "react";
 import "./SearchResults.css";
+import StarIcon from "@material-ui/icons/Star";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
-function SearchResults() {
-  return <div className="searchResults"></div>;
+function SearchResults({
+  img,
+  location,
+  title,
+  description,
+  star,
+  price,
+  total,
+}) {
+  return (
+    <div className="searchResults">
+      <img src={img} alt="pic" />
+      <FavoriteBorderIcon className="searchResults__heart" />
+      <div className="searchResults__info">
+        <div className="searchResults__infoTop">
+          <p>{location}</p>
+          <h3>{title}</h3>
+          <p>____</p>
+          <p>{description}</p>
+        </div>
+        <div className="searchResults__infoBottom">
+          <div className="searchResults__stars">
+            <StarIcon className="searchResults__star" />
+            <p>
+              <strong>{star}</strong>
+            </p>
+          </div>
+          <div className="searchResults__price">
+            <h2>{price}</h2>
+            <p>{total}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SearchResults;
