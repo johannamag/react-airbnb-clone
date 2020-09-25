97818453bf19760864e48c668a8e8053d0def210
johannamag <69107842+johannamag@users.noreply.github.com>
2020-09-25 13:11:00 +0200

    Added Card Content and Styling

--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Card.css";
 
-function Card() {
-  return <div className="card"></div>;
+function Card({ src, title, description, price }) {
+  return (
+    <div className="card">
+      <img src={src} />
+      <div className="card__info">
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
